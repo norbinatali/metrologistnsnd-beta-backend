6ecd34e0fc12e7f1f6d3276bae3fa14da36f01d1
Nataliia Hachok <norbinatali@gmail.com>
2019-12-14 16:45:40 +0200

    Update Device.ts

--- src/resolvers/Device.ts
+++ src/resolvers/Device.ts
@@ -4,4 +4,7 @@ export const Device = {
     author: ({id}, args, ctx: Context) => {
         return ctx.prisma.device({id}).author()
     },
+    tr: ({id}, args, ctx: Context) => {
+        return ctx.prisma.device({id}).tr()
+    },
 };
