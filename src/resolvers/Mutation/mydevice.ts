b52c141e42f9c7a20ef1592736bcedda82f1a31c
Nataliia Hachok <norbinatali@gmail.com>
2019-12-24 10:14:24 +0200

    Add files via upload

---
+++ src/resolvers/Mutation/mydevice.ts
@@ -0,0 +1,45 @@
+import {getUserId, Context, getTRId} from '../../utils'
+
+
+export const mydevice={
+createNewMyDevice: async function (parent, {brand_device,type_device, calibration,notes,verification_device, next_calibration, module_device}, ctx: Context, info){
+    const userId = getUserId(ctx);
+    return ctx.prisma.createMyDevice({
+        brand_device: brand_device,
+        type_device:  type_device,
+        module_device: module_device,
+        notes:notes,
+        verification_device:verification_device,
+        calibration: calibration,
+        next_calibration: next_calibration,
+        author:{connect:{id: userId}}
+
+    })
+},
+    async publishMyDevice(parent, { id }, ctx: Context, info) {
+        const userId = getUserId(ctx);
+        const mydeviceExists = await ctx.prisma.$exists.myDevice({
+            id,
+            author: { id: userId },
+        });
+        if (!mydeviceExists) {
+            throw new Error(`My device not found or you're not the author`)
+        }
+        return ctx.prisma.updateMyDevice({where: { id },data:{}
+        })
+    },
+    async deleteMyDevice(parent, { id }, ctx: Context, info) {
+        const userId = getUserId(ctx);
+        const mydeviceExists = await ctx.prisma.$exists.myDevice({
+            id,
+            author: { id: userId },
+        });
+        if (!mydeviceExists) {
+            throw new Error(`Post not found or you're not the author`)
+        }
+
+        return ctx.prisma.deleteMyDevice({ id })
+    },
+
+
+};
