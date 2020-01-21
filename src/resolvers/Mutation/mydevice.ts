06c5ba4da1157809123aed193258e05da906ecf0
Nataliia Hachok <norbinatali@gmail.com>
2020-01-21 09:48:25 +0200

    Update mydevice.ts

--- src/resolvers/Mutation/mydevice.ts
+++ src/resolvers/Mutation/mydevice.ts
@@ -2,20 +2,29 @@ import {getUserId, Context, getTRId} from '../../utils'
 
 
 export const mydevice={
-createNewMyDevice: async function (parent, {brand_device,type_device, calibration,notes,verification_device, next_calibration, module_device}, ctx: Context, info){
-    const userId = getUserId(ctx);
-    return ctx.prisma.createMyDevice({
-        brand_device: brand_device,
-        type_device:  type_device,
-        module_device: module_device,
-        notes:notes,
-        verification_device:verification_device,
-        calibration:calibration ,
-        next_calibration: next_calibration,
-        author:{connect:{id: userId}}
+createNewMyDevice: async function (parent, {conformity_data, name_device,series_device,brand_device,type_device,certification_conformity,certification_number, certification_calibration,department_center,notes,certification_verification, next_conformity,next_calibration, module_device, valid_verification, calibration}, ctx: Context, info){
+        const userId = getUserId(ctx);
+        return ctx.prisma.createMyDevice({
+            name_device: name_device,
+            certification_calibration:certification_calibration,
+            certification_verification:certification_verification,
+            certification_conformity:certification_conformity,
+            brand_device: brand_device,
+            type_device:  type_device,
+            series_device:series_device,
+            module_device: module_device,
+            notes:notes,
+            certification_number:certification_number,
+            conformity_data:conformity_data,
+            department_center:department_center,
+            next_conformity:next_conformity,
+            next_calibration: next_calibration,
+            valid_verification: valid_verification,
+            calibration: calibration,
+            author:{connect:{id: userId}}
 
-    })
-},
+        })
+    },
     async publishMyDevice(parent, { id }, ctx: Context, info) {
         const userId = getUserId(ctx);
         const mydeviceExists = await ctx.prisma.$exists.myDevice({
