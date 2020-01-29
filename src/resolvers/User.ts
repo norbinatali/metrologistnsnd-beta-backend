283dc79408d4a5b265ab64394ff3ab25ca216022
Nataliia Hachok <norbinatali@gmail.com>
2020-01-29 13:16:11 +0200

    Update User.ts

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -4,9 +4,7 @@ export const User = {
   posts: ({ id }, args, ctx: Context) => {
     return ctx.prisma.user({ id }).posts()
   },
-  devices: ({ id }, args, ctx: Context) => {
-    return ctx.prisma.user({ id }).devices()
-  },
+
   mydevices: ({ id }, args, ctx: Context) => {
     return ctx.prisma.user({ id }).mydevices()
   },
