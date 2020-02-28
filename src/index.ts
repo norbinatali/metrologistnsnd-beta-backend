a3701e4f2d17e9e8d3d737223114310463da2b32
Nataliia Hachok <norbinatali@gmail.com>
2020-02-28 11:55:30 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,7 @@ const server = new GraphQLServer({
 
 server.start({cors: {
       credentials: true,
-      origin: ["https://https://metrologistnsnd-beta-frontend.herokuapp.com"],
+      origin: ["https://metrologistnsnd-beta-frontend.herokuapp.com"],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204
+      
     }},() => console.log(`Server is running on http://localhost:4000`));
