07b5b9d75604ca014a704efb36128b98c3957d35
Nataliia Hachok <norbinatali@gmail.com>
2020-02-29 15:53:51 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,24 @@ import { GraphQLServer } from 'graphql-yoga'
 import { prisma } from './generated/prisma-client'
 import resolvers from './resolvers'
 import {ApolloServer} from 'apollo-server'
+import {importSchema} from 'graphql-import'
+
 const server = new ApolloServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
+  typeDefs: importSchema('/Users/norbinatali/Documents/MetrologistNSND/backend/src/schema.graphql'),
+  resolvers:resolvers,
   context: request => ({
     ...request,
     resolverValidationOptions: { requireResolversForResolveType: false,},
     prisma,
   }),
-  
-  
-})
+});
 
-server.start({cors: {
-      credentials: true,
-      origin: ["https://metrologistnsnd-beta-frontend.herokuapp.com"],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      
-    }},() => console.log(`Server is running on http://localhost:4000`));
+
+server.listen({
+  port: 4000, cors: {
+    credentials: true,
+    origin: ["https://metrologistnsnd-beta-frontend.herokuapp.com"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+
+  }
+}).then(({ url }) => {console.log(`Server is running on http://localhost:4000`)});
