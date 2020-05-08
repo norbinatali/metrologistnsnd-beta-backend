274f000a572b3440c8eaf789f02e054e5808c8a1
Nataliia Hachok <norbinatali@gmail.com>
2020-05-08 07:42:43 +0300

    Update team.ts

--- src/resolvers/Mutation/team.ts
+++ src/resolvers/Mutation/team.ts
@@ -13,30 +13,6 @@ export const team = {
             )
     },
 
-    async teamUser(parent, { id }, ctx: Context, info) {
-        const userId = getUserId(ctx);
-        const teamExists = await ctx.prisma.$exists.team({
-            id,
-            author: { id: userId },
-        });
-        if (!teamExists) {
-            throw new Error(`Team is not found or you're not the author`)
-        }
-
-        return ctx.prisma.updateTeam({where: { id },data:{}
-        })
-    },
-
-    async deleteTeam(parent, { id }, ctx: Context, info) {
-        const userId = getUserId(ctx);
-        const teamExists = await ctx.prisma.$exists.team({
-            id,
-            author: { id: userId },
-        });
-        if (!teamExists) {
-            throw new Error(`Post not found or you're not the author`)
-        }
-
-        return ctx.prisma.deleteTeam({ id })
-    },
+   
+   
 }
