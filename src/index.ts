2f9967d9f8da76e8f949d6a034e4231aa0fb1d6b
norbinatali <norbi>
2022-03-28 19:54:00 +0200

    correct cors in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,22 @@ import {prisma} from './generated/prisma-client'
 import resolvers from './resolvers'
 import {ApolloServer} from 'apollo-server'
 import {importSchema} from 'graphql-import'
+import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
 
 require('dotenv').config();
 
 const server = new ApolloServer({
-    cors: false,
+    cors: {
+        credentials: true,
+        origin: ['https://metrologistnsnd-beta-frontend.herokuapp.com', 'http://localhost:3000/'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+    },
     introspection: true,
     typeDefs: importSchema('./src/schema.graphql'),
     resolvers,
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
     context: request => ({
         ...request,
         resolverValidationOptions: {requireResolversForResolveType: false,},
