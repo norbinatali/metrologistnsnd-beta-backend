203359fd56a9ae63798c5122adcda6315d26509e
norbinatali <norbi>
2022-03-29 09:31:04 +0200

    correct cors in index.ts + express

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,47 @@
 import {prisma} from './generated/prisma-client'
 import resolvers from './resolvers'
-import {ApolloServer} from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express';
 import {importSchema} from 'graphql-import'
-import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
+import {ApolloServerPluginLandingPageGraphQLPlayground, ApolloServerPluginDrainHttpServer} from 'apollo-server-core';
+import * as express from 'express';
+import * as http from 'http';
 
 require('dotenv').config();
 
-const server = new ApolloServer({
-    cors: {
-        credentials: true,
-        origin: ['https://metrologistnsnd-beta-frontend.herokuapp.com', 'http://localhost:3000/'],
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
-    },
-    introspection: true,
-    typeDefs: importSchema('./src/schema.graphql'),
-    resolvers,
-    plugins: [
-        ApolloServerPluginLandingPageGraphQLPlayground(),
-    ],
-    context: request => ({
-        ...request,
-        resolverValidationOptions: {requireResolversForResolveType: false,},
-        prisma,
-    }),
-});
+async function startApolloServer() {
+    const app = express();
 
+    const httpServer = http.createServer(app);
 
-server.listen(process.env.PORT || 4000);
+    // CORS configuration
+    const corsOptions = {
+        origin: ['http://localhost:3000'],
+        credentials: true
+    }
+
+    const server = new ApolloServer({
+        typeDefs: importSchema('./src/schema.graphql'),
+        resolvers,
+        introspection: true,
+        context: request => ({
+            ...request,
+            resolverValidationOptions: {requireResolversForResolveType: false,},
+            prisma,
+        }),
+        plugins: [
+            ApolloServerPluginLandingPageGraphQLPlayground(),
+            ApolloServerPluginDrainHttpServer({ httpServer })
+        ],
+    });
+
+    await server.start();
+
+    server.applyMiddleware({ app, cors: corsOptions });
+
+    const PORT = process.env.PORT || 4000;
+
+    await new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve));
+    //console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startApolloServer();
